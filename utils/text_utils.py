252909107893442e5cb4585d1d4111d24668a3bc
Oliver Ainger <oainger@gmail.com>
2017-03-05 16:55:34 +0000

    Moved make_full_name function to text_utils module

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -44,3 +44,130 @@ def make_full_cd32_name(in_name):
                 in_name = replace_part
 
     return in_name
+
+
+def make_full_name(in_name):
+    # special "clean up" rules
+    in_name = in_name.replace("'n'", " 'n'")
+    in_name = in_name.replace("+", " +")
+    in_name = in_name.replace("&", " &")
+
+    old_name = in_name
+    in_name += "___"
+
+    first_length = len(in_name)
+
+    # special loop
+    #    for A in range(2, first_length - 3):
+    A = 1
+    B = len(old_name)
+
+    while A < len(in_name) and A < B:
+        A += 1
+
+        prev_char_2 = ord(mid_amos(in_name, A - 2, 1))
+        prev_char = ord(mid_amos(in_name, A - 1, 1))
+        this_char = ord(mid_amos(in_name, A, 1))
+        next_char = ord(mid_amos(in_name, A + 1, 1))
+        # never used?
+        # next_char_2 = ord(text_utils.mid_amos(in_name, A + 2, 1))
+
+        #  ===== add spaces
+
+        if 65 <= this_char <= 90:
+            # we are a capital letter
+
+            # special MB rule
+            if chr(this_char) == "M" and chr(next_char) == "B" and (48 <= this_char <= 57):
+                pass
+            # two underscores ... ignore
+            elif prev_char == 95 and prev_char_2 == 95:
+                pass
+            elif prev_char == 92 and prev_char_2 == 92:
+                pass
+            # previous is a capital A, but not part of AGA
+            elif prev_char == 65 and this_char != 71 and next_char != 65:
+                in_name = add_space(in_name, A)
+                A += 1
+                B += 1
+            # and the previous letter is not a space , and not also capital, or dash
+            elif prev_char != 32 and prev_char != 45 and not (65 <= prev_char <= 90):
+                in_name = add_space(in_name, A)
+                A += 1
+                B += 1
+
+        # we are a number
+        elif 48 <= this_char <= 57:
+            # and previous number was not a number and not a space
+            if not (48 <= prev_char <= 57) and prev_char != 32:
+                in_name = add_space(in_name, A)
+                A += 1
+                B += 1
+
+        if A > first_length:
+            break
+
+    # dirty manual fixes
+    in_name = in_name.replace("  ", " ")
+    in_name = in_name.replace("___", "")
+    in_name = in_name.replace("CD 32", "CD32")
+    in_name = in_name.replace(" CD32", " [CD32]")
+    in_name = in_name.replace(" CDTV", " [CDTV]")
+    in_name = in_name.replace(" AGA", " [AGA]")
+    in_name = in_name.replace(" 512 Kb", " (512Kb)")
+    in_name = in_name.replace(" 1 MB", " (1MB)")
+    in_name = in_name.replace(" 2 MB", " (2MB)")
+    in_name = in_name.replace(" 4 MB", " (4MB)")
+    in_name = in_name.replace(" 8 MB", " (8MB)")
+    in_name = in_name.replace(" 1 Disk", " (1 Disk)")
+    in_name = in_name.replace(" 2 Disk", " (2 Disk)")
+    in_name = in_name.replace(" 3 Disk", " (3 Disk)")
+    in_name = in_name.replace(" 4 Disk", " (4 Disk)")
+    in_name = in_name.replace(" Files", " (Files)")
+    in_name = in_name.replace(" Image", " (Image)")
+    in_name = in_name.replace(" Chip", " (Chip)")
+    in_name = in_name.replace(" Fast", " (Fast)")
+    in_name = in_name.replace("(Fast) Break", "Fast Break")
+    in_name = in_name.replace("R³sselsheim", "Russelsheim")
+
+    # check the txt file
+    file_name = "settings/WHD_Longname_Fixes.txt"
+    content = ""
+
+    if os.path.isfile(file_name) is True:
+        with open(file_name) as f:
+            content = f.readlines()
+            content = [x.strip() for x in content]
+        f.close()
+
+    for this_line in content:
+        if this_line.find("|") > -1:
+            find_part = left(this_line, this_line.find("|"))
+            replace_part = right(this_line, len(this_line) - this_line.find("|") - 1)
+
+            if in_name == find_part:
+                in_name = replace_part
+
+    # language rules
+    language = right(in_name, 3)
+
+    if language == " De":
+        in_name = left(in_name, len(in_name) - 3) + " (Deutsch)"
+    elif language == " Pl":
+        in_name = left(in_name, len(in_name) - 3) + " (Polski)"
+    elif language == " It":
+        in_name = left(in_name, len(in_name) - 3) + " (Italiano)"
+    elif language == " Dk":
+        in_name = left(in_name, len(in_name) - 3) + " (Dansk)"
+    elif language == " Es":
+        in_name = left(in_name, len(in_name) - 3) + " (Espanol)"
+    elif language == " Fr":
+        in_name = left(in_name, len(in_name) - 3) + " (Francais)"
+    elif language == " Cz":
+        in_name = left(in_name, len(in_name) - 3) + " (Czech)"
+    elif language == " Se":
+        in_name = left(in_name, len(in_name) - 3) + " (Svenska)"
+    elif language == " Fi":
+        in_name = left(in_name, len(in_name) - 3) + " (Finnish)"
+
+    return in_name
