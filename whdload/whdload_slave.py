ce14e21be63d14c83c3c27bbe07d2e42171559f2
HoraceAndTheSpider <horaceandthespider@hotmail.com>
2017-03-05 14:50:48 +0000

    'Fix' for creation date code which was Windows dependant.

--- whdload/whdload_slave.py
+++ whdload/whdload_slave.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import struct
 import binascii
 import re
@@ -28,7 +29,7 @@ class WHDLoadSlave:
 
     def __init__(self, path):
         self.path = path
-        self.created_time = time.ctime(os.path.getctime(path))
+        self.created_time = time.ctime(creation_date(path))
         self.size = None
         self.data_length = None
         self.data = None
@@ -200,3 +201,21 @@ class WHDLoadSlave:
                 except IndexError:
                     pass
         return False
+
+
+def creation_date(path_to_file):
+    """
+    Try to get the date that a file was created, falling back to when it was
+    last modified if that isn't possible.
+    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    """
+    if platform.system() == 'Windows':
+        return os.path.getctime(path_to_file)
+    else:
+        stat = os.stat(path_to_file)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            return stat.st_mtime
