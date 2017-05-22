8472d15c03441ed12e35c399bbae1abea2215110
HoraceAndTheSpider <horaceandthespider@hotmail.com>
2017-05-22 21:50:18 +0100

    Auto-startup generation added.

--- uae_config_maker.py
+++ uae_config_maker.py
@@ -274,7 +274,7 @@ def do_scan(input_directory, pathname,output_directory):
                 whd_page = ""
                
                # note that this *only* works with WHDLoad folder scanning.
-                if scan_mode == "WHDLoad":
+                if scan_mode == "WHDLoad" and WHDLOAD_UPDATE==True:
                     
                     whd_update_message = ""
                     for slave_file in glob.glob(file + "/*"):
@@ -324,8 +324,8 @@ def do_scan(input_directory, pathname,output_directory):
 #                             print(whd_realname + " === " + str(whd_infoname) + " === " + whd_page)
 
                             if whd_page == "":
-                                whd_update_message += whd_realname + " not found on master WHDLoad page list" + chr(10)
-                                print ("     Slave file: " + FontColours.OKBLUE + whd_realname + FontColours.ENDC + " not found on master WHDLoad page list" + chr(10))
+                                whd_update_message += whd_realname + " not found on master WHDLoad page list."  + chr(10) + "UAE Config Maker could not verify slave updates." + chr(10)
+                                print ("     Slave file: " + FontColours.OKBLUE + whd_realname + FontColours.ENDC + " not found on master WHDLoad page list." + chr(10))
                                 break
                             
                             # here's where we start checking again the WHDload page (OLLY!!!)
@@ -353,7 +353,10 @@ def do_scan(input_directory, pathname,output_directory):
                                 print("     This version: " + FontColours.FAIL + str(this_slave.modified_time) + FontColours.ENDC
                                                             + "  New Version: " +  FontColours.OKGREEN + str(web_slave.modified_time) + FontColours.ENDC)
                                 print()
-                                
+
+                                whd_update_message = whd_update_message + "     Slave file:  " + whd_realname + " is an older version." + chr(10) + chr(10)
+                                whd_update_message = whd_update_message + "     This version: " + str(this_slave.modified_time) + "" + chr(10)
+                                whd_update_message = whd_update_message + "     Detected Version: " +str(web_slave.modified_time) + "" + chr(10)
                                 # delete old slave
 
                                 # copy in new slave
@@ -606,7 +609,7 @@ def do_scan(input_directory, pathname,output_directory):
                 use_mouse2 = check_list("Control_Port1_Mouse.txt", this_file)
                 use_cd32_pad = check_list("Control_CD32.txt", this_file)
 
-                if scan_mode == "CD32":
+                if scan_mode == "CD32" or full_game_name.lower().find("[cd32]") > -1:
                     use_cd32_pad = True
 
                 # '======== MISC SETTINGS =======
@@ -631,16 +634,16 @@ def do_scan(input_directory, pathname,output_directory):
                 # ' ....
 
                 # print("we are making a config ....")
-                file = output_directory + full_game_name + ".uae"
-                shutil.copyfile("uaeconfig.uaetemp", file)
+                config_file = output_directory + full_game_name + ".uae"
+                shutil.copyfile("uaeconfig.uaetemp", config_file)
 
-                if os.path.isfile(file) is False:
+                if os.path.isfile(config_file) is False:
                     print(FontColours.FAIL + "Error creating config." + FontColours.ENDC)
                 else:
-                    print("     Editing File: " + FontColours.HEADER + file + FontColours.ENDC)
+                    print("     Editing File: " + FontColours.HEADER + config_file + FontColours.ENDC)
 
                     # put the text from the file into a string
-                    text_file = open(file, "r")
+                    text_file = open(config_file, "r")
                     config_text = text_file.read()
                     text_file.close()
 
@@ -789,10 +792,67 @@ def do_scan(input_directory, pathname,output_directory):
                         config_text = config_text.replace("<<port1mode>>", "djoy")
 
                     # save out the config changes
-                    text_file = open(file, "w")
+                    text_file = open(config_file, "w")
                     text_file.write(config_text)
                     text_file.close()
+                    
+                ## this point we can jump in, having created a config.
 
+                # we'll need to check we are on WHDLoad mode and creation of auto-startup is ON
+                if scan_mode == "WHDLoad" and CREATE_AUTOSTARTUP == True:                   
+                    slave_count = 0
+                    for slave_file in glob.glob(file + "/*"):
+                        if slave_file.lower().endswith(".slave"):
+                            
+                            this_slave = whdload_slave.whdload_factory(slave_file)
+                            slave_count += 1
+
+                # then check that there is 1 slave file only
+                    if slave_count == 1 and os.path.isfile(file + "/auto-startup") == False:
+
+                    # if so, we can make an auto-startup file
+                        data_path = (this_slave.current_dir)
+                        if data_path !="": data_path += "/"
+                        
+                        autostart_text = 'CD "WHDLoadGame:"' + chr(10)
+                        autostart_text += 'WHDLOAD SLAVE="WHDLoadGame:' + this_slave.file_name
+                        autostart_text += ' PRELOAD NOWRITECACHE NOREQ SPLASHDELAY=0'
+                        autostart_text += ' data="WHDLoadGame:' + data_path +'"'
+                        autostart_text += ' NOREQ >"WHDLoadGame:whdscript_debug"' + chr(10)
+                        autostart_text += 'uae-configuration SPC_QUIT 1' + chr(10)
+
+                        #print (autostart_text)
+                        
+                        text_file = open(file + "/auto-startup", "w+")
+                        text_file.write(autostart_text)
+                        text_file.close()
+
+
+                    # we will also leave a message about kickstarts
+                        if len(this_slave.kickstarts) > 0:
+                            kick_text = ""
+
+                            kick_text += "This WHDLoad slave cannot be run without one of the " + chr(10)
+                            kick_text += "following kickstart roms in BootWHD:devs/kickstarts/" + chr(10) + chr(10)
+                            
+                            for kick in (this_slave.kickstarts):
+                                kick_text += "kick" + kick.name + chr(10)
+
+                            kick_text += chr(10) + "Should this game fail to load, please check for these files." + chr(10)
+                            #print (kick_text)
+                            
+                            text_file = open(file + "/whdkickstarts_message", "w+")
+                            text_file.write(kick_text)
+                            text_file.close()
+  
+                            #whdupdate_message
+                            #whdscript_debug
+
+                        print()
+                        print (FontColours.OKBLUE + "     Created 'auto-startup' file." + FontColours.ENDC)
+                                   
+            # this point we can jump in, config creation or not
+            
             print()
             count += 1
 
@@ -838,7 +898,7 @@ def do_scan_base(inputdir,outputdir):
 print()
 print(
     FontColours.BOLD + FontColours.OKBLUE + "HoraceAndTheSpider" + FontColours.ENDC + "'s " + FontColours.BOLD +
-    "UAE Configuration Maker" + FontColours.ENDC + FontColours.OKGREEN + " (v2.1)" + FontColours.ENDC + " | " + "" +
+    "UAE Configuration Maker" + FontColours.ENDC + FontColours.OKGREEN + " (2.3)" + FontColours.ENDC + " | " + "" +
     FontColours.FAIL + "www.ultimateamiga.co.uk" + FontColours.ENDC)
 print()
 
@@ -877,6 +937,16 @@ parser.add_argument('--force-config-overwrite',  # command line argument
 parser.add_argument('--force-pi-paths',  # command line argument
                     action="store_true",  # if argument present, store value as True otherwise False
                     help="Force RetroPie Paths"
+                    )
+
+parser.add_argument('--whdload-update',  # command line argument
+                    action="store_true",  # if argument present, store value as True otherwise False
+                    help="Check for WHDLoad Updates"
+                    )
+
+parser.add_argument('--create-autostartup',  # command line argument
+                    action="store_true",  # if argument present, store value as True otherwise False
+                    help="Generate auto-startup file for WHDLoad folders"
                     )
 
 # Parse all command line arguments
@@ -928,8 +998,22 @@ if text_utils.str2bool(find_host_option("force_config_overwrite")) == True : FOR
 # >> Setup Bool Constant for Pi Paths
 FORCE_PI_PATHS = args.force_pi_paths
 
-# if hostconfig specifies force_config_overwrite, use as override
+# if hostconfig specifies ..., use as override
 if text_utils.str2bool(find_host_option("force_pi_paths")) == True : FORCE_PI_PATHS = True
+
+
+# >> Setup Bool Constant for WHDLOAD Slave updating
+WHDLOAD_UPDATE = args.whdload_update
+
+# if hostconfig specifies ...., use as override
+if text_utils.str2bool(find_host_option("whdload_update")) == True : WHDLOAD_UPDATE = True
+
+
+# >> Setup Bool Constant for WHDLOAD Slave updating
+CREATE_AUTOSTARTUP = args.create_autostartup
+
+# if hostconfig specifies ...., use as override
+if text_utils.str2bool(find_host_option("create_autostartup")) == True : CREATE_AUTOSTARTUP = True
 
 
 # paths/folders if needed
